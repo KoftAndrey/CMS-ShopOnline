c31bb915b1047866d3c1344e4f8362555f5fb179
Andrey <KoftAndrey@gmail.com>
2022-12-12 15:11:46 +0300

    module 4 lesson 1

---
+++ assets/style/global/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+    let productName = 'Сорочка мужская';
+    let productCount = 1;
+    const productCategory =  'Одежда';
+    let productPrice = 10000;
+
+    console.log (productName);
+    console.log (productCount * productPrice);
+}
