d2b2ab3aa5a0624e85b8fc7b39a32e70a3c0e730
Andrey <KoftAndrey@gmail.com>
2023-06-14 00:22:48 +0300

    edited after comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,5 +3,6 @@
 const modalHeader = document.querySelector('.modal__title');
 const modalId = document.querySelector('.modal__id');
 const modalForm = document.querySelector('.modal__form');
-const modalCheckbox = document.querySelector('.form__checkbox-container');
+const modalCheckboxButton = document.querySelector('.form__checkbox');
+const modalCheckboxInput = document.querySelector('.form__input');
 const modalFinalCost = document.querySelector('.modal__total-price_content_value');
